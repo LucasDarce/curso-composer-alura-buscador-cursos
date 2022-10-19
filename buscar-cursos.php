7f51ad1773010c19012cd041c14a0e3937300b77
Lucas Darce <lucas.filetti@autenticacertificado.com.br>
2022-10-19 11:44:28 -0300

    Primeiro commit

---
+++ buscar-cursos.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+function exibeMensagem (string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+$httpClient = new \GuzzleHttp\Client([
+    'base_uri' => 'https://www.alura.com.br/',
+    'verify' => false
+]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($httpClient, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
